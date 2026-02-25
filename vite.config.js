4ab03678e1e30ccb19d8c9891a931e15e68a2d5f
nerellabhavya <nerellabhavyasri@gmail.com>
2026-02-25 23:04:13 +0530

    Update base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ItzFizz-InfoTech-Interview1/', // Add this line!
 })
